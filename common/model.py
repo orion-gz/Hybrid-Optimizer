11992407b852bb35d5bf0ac39cbeeb84d21d663d
orion_gz <asdfg60842@gmail.com>
2026-04-14 17:58:03 +0900

    sync: 2026-04-14 17:58:03

--- common/model.py
+++ common/model.py
@@ -1,10 +1,12 @@
 import torch
 import torch.nn as nn
 
+
 class WideBasicBlock(nn.Module):
+    """Basic residual block for WideResNet using the pre-activation (BN-ReLU-Conv) order."""
+
     def __init__(self, in_planes, out_planes, stride, dropout_rate):
         super(WideBasicBlock, self).__init__()
-        # BN-ReLU-Conv order instead of the original ResNet Conv-BN-ReLU
         self.bn1 = nn.BatchNorm2d(in_planes)
         self.relu1 = nn.ReLU(inplace=True)
         self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=3, padding=1, bias=False)
@@ -12,28 +14,30 @@ class WideBasicBlock(nn.Module):
         self.relu2 = nn.ReLU(inplace=True)
         self.dropout = nn.Dropout(p=dropout_rate)
         self.conv2 = nn.Conv2d(out_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        
+
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != out_planes:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
             )
-            
+
     def forward(self, x):
         out = self.dropout(self.conv1(self.relu1(self.bn1(x))))
         out = self.conv2(self.relu2(self.bn2(out)))
         out += self.shortcut(x)
         return out
 
-# %%
+
 class WideResNet(nn.Module):
+    """WideResNet with configurable depth, widening factor, dropout, and number of classes."""
+
     def __init__(self, depth, widen_factor, dropout_rate, num_classes):
         super(WideResNet, self).__init__()
         self.in_planes = 16
         n = (depth - 4) // 6
         k = widen_factor
         n_stages = [16, 16 * k, 32 * k, 64 * k]
-        
+
         self.conv1 = nn.Conv2d(3, n_stages[0], kernel_size=3, padding=1, bias=False)
         self.layer1 = self._make_layer(WideBasicBlock, n_stages[1], n, dropout_rate, stride=1)
         self.layer2 = self._make_layer(WideBasicBlock, n_stages[2], n, dropout_rate, stride=2)
@@ -42,7 +46,7 @@ class WideResNet(nn.Module):
         self.relu = nn.ReLU(inplace=True)
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(n_stages[3], num_classes)
-        
+
     def _make_layer(self, block, planes, num_blocks, dropout_rate, stride):
         strides = [stride] + [1] * (num_blocks - 1)
         layers = []
@@ -61,6 +65,8 @@ class WideResNet(nn.Module):
         out = out.view(out.size(0), -1)
         out = self.fc(out)
         return out
-    
+
+
 def WRN_28_10(num_classes=10, dropout_rate=0.3):
+    """Instantiate WideResNet-28-10."""
     return WideResNet(depth=28, widen_factor=10, dropout_rate=dropout_rate, num_classes=num_classes)
