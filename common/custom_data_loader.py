30b5359a463ca513691a307e6045f059ce75e599
orion_gz <asdfg60842@gmail.com>
2026-04-14 18:00:22 +0900

    sync: 2026-04-14 18:00:22

--- common/custom_data_loader.py
+++ common/custom_data_loader.py
@@ -68,7 +68,7 @@ class UnifiedImageFolder(Dataset):
         return sample, target
 
 def get_data_loaders(config):
-    # ImageNet 표준 정규화
+    # ImageNet
     mean = [0.485, 0.456, 0.406]
     std = [0.229, 0.224, 0.225]
     
