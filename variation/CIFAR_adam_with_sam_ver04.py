bff40d97da211f6cac7b5dda41b3b56cf71e73e9
orion_gz <asdfg60842@gmail.com>
2026-04-14 18:10:44 +0900

    sync: 2026-04-14 18:10:44

--- variation/CIFAR_adam_with_sam_ver04.py
+++ variation/CIFAR_adam_with_sam_ver04.py
@@ -99,13 +99,13 @@ def get_data_loaders(config):
         test_set = dataset_class(root=config['data_path'], train=False, download=True, transform=test_transform)
 
     train_size = int(0.8 * len(full_train))
-    val_size   = len(full_train) - train_size
-    gen        = torch.Generator().manual_seed(config['seed'])
+    val_size = len(full_train) - train_size
+    gen = torch.Generator().manual_seed(config['seed'])
     train_set, val_set = random_split(full_train, [train_size, val_size], generator=gen)
 
     train_loader = DataLoader(train_set, batch_size=config['batch_size'], shuffle=True,  num_workers=config['num_workers'])
-    val_loader   = DataLoader(val_set,   batch_size=config['batch_size'], shuffle=False, num_workers=config['num_workers'])
-    test_loader  = DataLoader(test_set,  batch_size=config['batch_size'], shuffle=False, num_workers=config['num_workers'])
+    val_loader = DataLoader(val_set,   batch_size=config['batch_size'], shuffle=False, num_workers=config['num_workers'])
+    test_loader = DataLoader(test_set,  batch_size=config['batch_size'], shuffle=False, num_workers=config['num_workers'])
 
     return train_loader, val_loader, test_loader
 
